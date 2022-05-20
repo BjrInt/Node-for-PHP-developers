9f83db00324c0d0473194e7127cd8af25edfeed2
BonjourInternet <bonjour@bonjourinternet.top>
2022-05-20 11:09:54 +0200

    enh: good practices

--- 1.open_a_file/2.good.mjs
+++ 1.open_a_file/2.good.mjs
@@ -1,11 +1,11 @@
-import { readFile } from "fs/promises"
+import { readFile } from 'fs/promises'
 
 const displayFileLength = async () => {
   try {
     const content = await readFile()
     return content.length
   } catch (err) {
-    return err
+    console.log(0)
+    return 0
   }
 }
-
